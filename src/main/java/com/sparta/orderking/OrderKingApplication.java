c4ddb3d55f2d2ecccb6fd83b950f078b31ddd9f4
checkuu-0216 <corb37@gmail.com>
2024-09-19 13:40:42 +0900

    첫커밋

---
+++ src/main/java/com/sparta/orderking/OrderKingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.orderking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/orderking/OrderKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.orderking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
