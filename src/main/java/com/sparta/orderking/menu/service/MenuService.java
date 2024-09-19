0a4e4caa00dc16b1c4141e343e5225bcbf8f82d6
checkuu-0216 <corb37@gmail.com>
2024-09-19 15:42:15 +0900

    [ADD] pull request 테스트3

--- src/main/java/com/sparta/orderking/menu/service/MenuService.java
+++ src/main/java/com/sparta/orderking/menu/service/MenuService.java
@@ -1,4 +1,9 @@
 package com.sparta.orderking.menu.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MenuService {
 }
