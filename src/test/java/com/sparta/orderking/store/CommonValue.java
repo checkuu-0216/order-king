726360589655b18a785d800f19037a0e899fb298
checkuu-0216 <corb37@gmail.com>
2024-09-19 22:35:26 +0900

    [refactory] merge 오류

--- src/test/java/com/sparta/orderking/store/CommonValue.java
+++ src/test/java/com/sparta/orderking/store/CommonValue.java
@@ -1,17 +1,17 @@
 package com.sparta.orderking.store;
 
 import com.sparta.orderking.config.AuthUser;
-import com.sparta.orderking.config.UserRoleEnum;
 import com.sparta.orderking.domain.store.dto.StoreRequestDto;
 import com.sparta.orderking.domain.store.dto.StoreResponseDto;
 import com.sparta.orderking.domain.store.entity.Store;
 import com.sparta.orderking.domain.store.entity.StoreStatus;
+import com.sparta.orderking.domain.user.entity.UserEnum;
 
 public class CommonValue {
     public final static StoreRequestDto TEST_STOREREQUESTDTO = new StoreRequestDto("name","storeAddress","storeNumber",10000,9,21, StoreStatus.OPEN);
     public final static Store TEST_STORE = new Store(1L,"name","storeAddress","storeNumber",10000,9,21, StoreStatus.OPEN);
     public final static Store TEST_STORE2 = new Store(1L,"name","storeAddress","storeNumber",10000,9,21, StoreStatus.CLOSED);
     public final static StoreResponseDto TEST_STORERESPONSEDTO = new StoreResponseDto(TEST_STORE);
-    public final static AuthUser TEST_AUTHUSER2 = new AuthUser(1L, UserRoleEnum.USER);
-    public final static AuthUser TEST_AUTHUSER = new AuthUser(1L, UserRoleEnum.OWNER);
+    public final static AuthUser TEST_AUTHUSER2 = new AuthUser(1L, UserEnum.USER);
+    public final static AuthUser TEST_AUTHUSER = new AuthUser(1L, UserEnum.OWNER);
 }
