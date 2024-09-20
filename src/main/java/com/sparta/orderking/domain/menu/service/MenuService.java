a5b99e6e3bde7822a76f9d31436335e559559f6e
checkuu-0216 <corb37@gmail.com>
2024-09-20 13:20:02 +0900

    [refactory] menuService user확인 부분 수정

--- src/main/java/com/sparta/orderking/domain/menu/service/MenuService.java
+++ src/main/java/com/sparta/orderking/domain/menu/service/MenuService.java
@@ -8,6 +8,9 @@ import com.sparta.orderking.domain.menu.entity.MenuPossibleEnum;
 import com.sparta.orderking.domain.menu.repository.MenuRepository;
 import com.sparta.orderking.domain.store.entity.Store;
 import com.sparta.orderking.domain.store.repository.StoreRepository;
+import com.sparta.orderking.domain.user.entity.User;
+import com.sparta.orderking.domain.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +22,14 @@ public class MenuService {
 
     private final MenuRepository menuRepository;
     private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
 
     public void saveMenu(AuthUser authUser, Long storeId, MenuRequestDto requestDto) {
         //해당 가게 존재 확인
         Store store = storeRepository.findById(storeId).orElseThrow(() -> new NullPointerException("해당 가게가 존재하지 않습니다."));
         //해당 가게 주인이 맞는지 확인
-        storeRepository.findByUserAndStore(authUser, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재 하지 않습니다."));
+        storeRepository.findByStoreAndUser(user, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
         //등록할 메뉴 생성
         Menu menu = new Menu(requestDto);
         //메뉴 저장
@@ -35,7 +40,8 @@ public class MenuService {
         //해당 가게 확인
         Store store = storeRepository.findById(storeId).orElseThrow(() -> new NullPointerException("해당 가게가 존재하지 않습니다."));
         //해당 가게 주인이 맞는지 확인
-        storeRepository.findByUserAndStore(authUser, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재 하지 않습니다."));
+        storeRepository.findByStoreAndUser(user, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
         //등록되어있는지 메뉴 확인
         Menu menu = menuRepository.findById(menuId).orElseThrow(() -> new NullPointerException("해당 메뉴가 존재 하지 않습니다."));
         //업데이트 된 request를 받아서 저장
@@ -47,11 +53,12 @@ public class MenuService {
         //해당 가게 확인
         Store store = storeRepository.findById(storeId).orElseThrow(() -> new NullPointerException("해당 가게가 존재하지 않습니다."));
         //해당 가게 주인이 맞는지 확인
-        storeRepository.findByUserAndStore(authUser, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재 하지 않습니다."));
+        storeRepository.findByStoreAndUser(user, store).orElseThrow(() -> new NullPointerException("해당 가게 주인이 아닙니다."));
         //등록되어있는지 메뉴 확인
-        Menu menu = menuRepository.findById(menuId).orElseThrow(() -> new NullPointerException("해당 메뉴가 존재 하지 않습니다."));
-        //등록되어있는 메뉴 상태 변화
+        Menu menu = menuRepository.findById(menuId).orElseThrow(()->new NullPointerException("해당 메뉴가 존재 하지 않습니다."));
         menu.deleteMenu(DELETE);
+        //등록되어있는 메뉴 상태 변화
         menuRepository.save(menu);
     }
 }
--- src/main/java/com/sparta/orderking/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/orderking/domain/store/repository/StoreRepository.java
@@ -2,10 +2,13 @@ package com.sparta.orderking.domain.store.repository;
 
 import com.sparta.orderking.domain.store.entity.Store;
 import com.sparta.orderking.domain.store.entity.StoreStatus;
+import com.sparta.orderking.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
     List<Store> findByNameAndStoreStatus(String name, StoreStatus serviceEnum);
+    Optional<Store> findByStoreAndUser(User user, Store store);
 }
