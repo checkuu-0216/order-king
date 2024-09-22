63dc204d5b1af126595231fff2182fd6cc51c13d
wjdtmdgjs-1 <jeong5207@naver.com>
2024-09-22 09:13:18 +0900

    [add]BookmakrController testcode

--- src/main/java/com/sparta/orderking/domain/store/controller/BookMarkController.java
+++ src/main/java/com/sparta/orderking/domain/store/controller/BookMarkController.java
@@ -15,12 +15,12 @@ public class BookMarkController {
 
     private final BookmarkService bookmarkService;
 
-    @PostMapping("/stores/{storeId}/bookmarkon")
+    @PostMapping("/{storeId}/bookmarkon")
     public ResponseEntity<BookmarkSaveResponseDto> bookmarkOn(@Auth AuthUser authUser, @PathVariable long storeId) {
         return ResponseEntity.ok(bookmarkService.bookmarkOn(authUser, storeId));
     }
 
-    @DeleteMapping("/stores/{storeId}/bookmarkoff")
+    @DeleteMapping("/{storeId}/bookmarkoff")
     public void bookmarkOff(@Auth AuthUser authUser, @PathVariable long storeId) {
         bookmarkService.bookmarkOff(authUser, storeId);
     }
