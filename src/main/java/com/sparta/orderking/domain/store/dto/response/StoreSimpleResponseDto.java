b6ed8e7e937bb2faf6a5176007ff6c24eadb6e4a
checkuu-0216 <corb37@gmail.com>
2024-09-23 23:07:21 +0900

    [fix] 검색기능 코드 수정

--- src/main/java/com/sparta/orderking/domain/store/dto/response/StoreSimpleResponseDto.java
+++ src/main/java/com/sparta/orderking/domain/store/dto/response/StoreSimpleResponseDto.java
@@ -1,12 +1,17 @@
 package com.sparta.orderking.domain.store.dto.response;
 
+import com.sparta.orderking.domain.menu.dto.MenuSimpleResponseDto;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class StoreSimpleResponseDto {
     private final String name;
+    private final List<String> menus;
 
-    public StoreSimpleResponseDto(String name) {
+    public StoreSimpleResponseDto(String name, List<String> menus) {
         this.name = name;
+        this.menus = menus;
     }
 }
--- src/main/java/com/sparta/orderking/domain/store/service/SearchStoreService.java
+++ src/main/java/com/sparta/orderking/domain/store/service/SearchStoreService.java
@@ -1,5 +1,8 @@
 package com.sparta.orderking.domain.store.service;
 
+import com.sparta.orderking.domain.menu.dto.MenuResponseDto;
+import com.sparta.orderking.domain.menu.dto.MenuSimpleResponseDto;
+import com.sparta.orderking.domain.menu.entity.Menu;
 import com.sparta.orderking.domain.menu.repository.MenuRepository;
 import com.sparta.orderking.domain.store.dto.response.StoreResponseDto;
 import com.sparta.orderking.domain.store.dto.response.StoreSimpleResponseDto;
@@ -8,10 +11,7 @@ import com.sparta.orderking.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,21 +24,37 @@ public class SearchStoreService {
         List<Store> storeByName = storeRepository.findByNameContaining(keyword);
         List<Store> storeByMenu = storeRepository.findStoreByMenuName(keyword);
 
-        //키워드 포함된 가게 저장하기
-        Set<StoreSimpleResponseDto> result = new HashSet<>();
+        Map<String, StoreSimpleResponseDto> resultMap = new HashMap<>();
+
         for (Store store : storeByName) {
-            StoreSimpleResponseDto dto = new StoreSimpleResponseDto(store.getName());
-            result.add(dto);
-        }
-        //키워드를 포함한 메뉴를 가진 가게 저장하기
-        Set<StoreSimpleResponseDto> storeNameByMenu = new HashSet<>();
-        for (Store menu : storeByMenu) {
-            StoreSimpleResponseDto dto = new StoreSimpleResponseDto(menu.getName());
-            storeNameByMenu.add(dto);
+            resultMap.put(store.getName(), new StoreSimpleResponseDto(store.getName(), new ArrayList<>()));
         }
 
-        //검색된 결과 병합
-        result.addAll(storeNameByMenu);
-        return new ArrayList<>(result);
+        // 메뉴로 추가
+        for (Store menuStore : storeByMenu) {
+            for (Menu menu : menuStore.getMenus()) { // 가게의 모든 메뉴를 순회
+                resultMap.computeIfPresent(menuStore.getName(), (key, dto) -> {
+                    dto.getMenus().add(menu.getMenuName()); // 실제 메뉴 이름을 추가
+                    return dto;
+                });
+            }
+        }
+        return new ArrayList<>(resultMap.values());
     }
 }
+    //        //키워드 포함된 가게 저장하기
+//        Set<StoreSimpleResponseDto> result = new HashSet<>();
+//        for (Store store : storeByName) {
+//            StoreSimpleResponseDto dto = new StoreSimpleResponseDto(store.getName());
+//            result.add(dto);
+//        }
+    //키워드를 포함한 메뉴를 가진 가게 저장하기
+//        Set<StoreSimpleResponseDto> storeNameByMenu = new HashSet<>();
+//        for (Store menu : storeByMenu) {
+//            StoreSimpleResponseDto dto = new StoreSimpleResponseDto(menu.getName());
+//            storeNameByMenu.add(dto);
+//        }
+    //검색된 결과 병합
+//        result.addAll(storeNameByMenu);
+//        return new ArrayList<>(result);
+
