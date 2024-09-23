d6d5f9a6b43f67bef70c947bf9ca75323f0bd6a7
jhwook <jhwooook@naver.com>
2024-09-23 10:57:19 +0900

    [fix] 주문 생성/상태변경 AOP 수정

--- src/main/java/com/sparta/orderking/aop/OrderLogging.java
+++ src/main/java/com/sparta/orderking/aop/OrderLogging.java
@@ -15,33 +15,30 @@ import java.time.LocalDateTime;
 @Slf4j
 public class OrderLogging {
     // 주문 생성 및 상태 변경 메서드를 지정하는 포인트컷
-    @Pointcut("execution(* com.sparta.orderking.domain.order.controller.OrderController.createOrder(..)) " +
-            "|| execution(* com.sparta.orderking.domain.order.controller.OrderController.updateOrderStatus(..))")
-    public void orderMethods() {}
+    @Pointcut("execution(* com.sparta.orderking.domain.order.controller.OrderController.createOrder(..)) ")
+    public void orderCreateMethods() {}
 
-    // 주문 생성/상태 변경 이전에 로그 기록
-    @Before("orderMethods()")
-    public void logBefore(JoinPoint joinPoint) {
+    @Pointcut("execution(* com.sparta.orderking.domain.order.controller.OrderController.updateOrderStatus(..))")
+    public void orderUpdateMethods() {}
+
+    @AfterReturning("orderCreateMethods()")
+    public void orderCreateLogAfterReturning(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
-        Long storeId = (Long) args[0]; // 가게 ID
-        Long orderId = (Long) args[1]; // 주문 ID
+        Long storeId = (Long) args[1]; // 가게 ID
 
-        // 로그 출력
-        log.info("주문 생성/상태 변경 요청이 생겼습니다.");
+        log.info("주문 요청이 생겼습니다.");
         log.info("요청 시각: " + LocalDateTime.now());
         log.info("가게 ID: " + storeId);
-        log.info("주문 ID: " + orderId);
     }
 
-    // 주문 상태가 변경된 후에 로그 기록
-    @AfterReturning("orderMethods()")
-    public void logAfter(JoinPoint joinPoint) {
+    @AfterReturning("orderUpdateMethods()")
+    public void orderUpdateLogAfterReturning(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
-        Long storeId = (Long) args[0];
-        Long orderId = (Long) args[1];
+        Long storeId = (Long) args[1]; // 가게 ID
+        Long orderId = (Long) args[2]; // 주문 ID
 
-        // 상태 변경 로그 출력
-        log.info("주문 상태가 성공적으로 변경됨");
+        log.info("주문 상태 변경 요청이 생겼습니다.");
+        log.info("요청 시각: " + LocalDateTime.now());
         log.info("가게 ID: " + storeId);
         log.info("주문 ID: " + orderId);
     }
